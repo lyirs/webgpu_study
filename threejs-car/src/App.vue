a82d028bb3391174edff6150e26e57a299c38f94
lv <992526373@qq.com>
2023-06-21 16:29:23 +0800

    feat：template

--- threejs-car/src/App.vue
+++ threejs-car/src/App.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+// @ts-ignore
 import WebGPURenderer from "three/examples/jsm/renderers/webgpu/WebGPURenderer.js";
+// @ts-ignore
 import WebGPU from "three/examples/jsm/capabilities/WebGPU.js";
 import * as Nodes from "three/examples/jsm/nodes/Nodes.js";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+// @ts-ignore
 import { color } from "three/nodes";
 
 if (WebGPU.isAvailable() === false) {
@@ -33,6 +36,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 // 添加渲染器到DOM
 document.body.appendChild(renderer.domElement);
 // 创建相机控制器
+// @ts-ignore
 const controls = new OrbitControls(camera, renderer.domElement);
 
 // 渲染场景
@@ -62,6 +66,7 @@ skybox.position.set(0, 10, 0);
 scene.add(skybox);
 
 // 地面材质纹理
+// @ts-ignore
 const uvNode = Nodes.uv().mul(5);
 const fabricTexture = textureLoader.load(
   "texture/fabric/FabricPlainWhiteBlackout009_COL_2K.jpg"
@@ -84,14 +89,16 @@ const planeGeometry = new THREE.CircleGeometry(40, 64);
 const planeMaterial = new Nodes.MeshPhysicalNodeMaterial({
   side: THREE.DoubleSide,
 });
-
+// @ts-ignore
 planeMaterial.colorNode = Nodes.texture(fabricTexture, uvNode).mul(
   Nodes.texture(
     aoTexture,
+    // @ts-ignore
     Nodes.uv().mul(Nodes.vec2(30, 14)).add(Nodes.vec2(-14.525, -6.52))
   )
 );
 planeMaterial.normalNode = Nodes.texture(fabricNormalMap, uvNode);
+// @ts-ignore
 planeMaterial.sheenNode = Nodes.texture(fabricSheen, uvNode);
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 plane.rotation.x = -Math.PI / 2;
@@ -105,6 +112,9 @@ spotLight.position.set(0, 10, 0);
 spotLight.angle = Math.PI / 8;
 spotLight.penumbra = 0.3;
 scene.add(spotLight);
+
+// 创建视频对象
+const video = document.querySelector("#video") as HTMLVideoElement;
 
 // 加载模型
 const loader = new GLTFLoader();
@@ -137,14 +147,75 @@ loader.load("model/zeekr.glb", (gltf) => {
         child.material.clearcoat = 1;
         child.material.clearcoatRoughness = 0;
       }
+      if (child.material && child.material.name == "视频纹理") {
+        video.play();
+        let videoTexture = new THREE.VideoTexture(video);
+        child.material = new THREE.MeshPhongMaterial({
+          map: videoTexture,
+          emissiveMap: videoTexture,
+        });
+        // video.addEventListener("loadeddata", () => {
+        //   video.play();
+        //   let videoTexture = new THREE.VideoTexture(video);
+        //   child.material = new THREE.MeshPhongMaterial({
+        //     map: videoTexture,
+        //     emissiveMap: videoTexture,
+        //   });
+        // });
+      }
     }
   });
   scene.add(model);
 });
+
+// 添加点光源
+const pointLight = new THREE.PointLight(0xff9999, 1);
+pointLight.position.set(4, 4, 0);
+scene.add(pointLight);
+
+const pointLight2 = new THREE.PointLight(0x99ffff, 1);
+pointLight2.position.set(-4, 4, 0);
+scene.add(pointLight2);
+
+const pointLight3 = new THREE.PointLight(0xff99ff, 1);
+pointLight3.position.set(0, 4, 4);
+scene.add(pointLight3);
+
+const pointLight4 = new THREE.PointLight(0x00ffff, 1);
+pointLight4.position.set(0, 4, -4);
+scene.add(pointLight4);
 </script>
 
 <template>
-  <div></div>
+  <div>
+    <div class="colors" v-show="isColor">
+      <div
+        class="color"
+        v-for="(item, index) in colors"
+        :key="index"
+        :style="{ backgroundColor: item.color }"
+        @click="changeColor(item.color)"
+      ></div>
+    </div>
+    <div class="tab">
+      <div class="btn" @click="toggleColor">
+        <div class="btn-icon1"></div>
+        <div class="btn-text">颜色</div>
+      </div>
+      <div class="btn" @click="enterCar">
+        <div class="btn-icon2"></div>
+        <div class="btn-text">内饰</div>
+      </div>
+      <div class="btn" @click="outCar">
+        <div class="btn-icon3"></div>
+        <div class="btn-text">车身</div>
+      </div>
+      <div class="btn" @click="openDoor">
+        <div class="btn-icon3"></div>
+        <div class="btn-text">开车门</div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style scoped></style>
